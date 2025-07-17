84f0e61d8e16d01819da779fb6cba7bc1931ee11
Anurag Pandey <panurag8127@gmail.com>
2025-07-17 15:30:56 +0530

    Create LoginPage.java

---
+++ src/test/java/com/example/Pages/LoginPage.java
@@ -0,0 +1,37 @@
+package com.example.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    private WebDriver driver;
+
+    @FindBy(id = "email")
+    private WebElement emailField;
+
+    @FindBy(id = "passwd")
+    private WebElement passwordField;
+
+    @FindBy(id = "SubmitLogin")
+    private WebElement submitButton;
+
+    @FindBy(className = "logout")
+    private WebElement logoutLink;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void login(String email, String password) {
+        emailField.sendKeys(email);
+        passwordField.sendKeys(password);
+        submitButton.click();
+    }
+
+    public boolean isLoggedIn() {
+        return logoutLink.isDisplayed();
+    }
+}
